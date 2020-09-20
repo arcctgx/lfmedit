f8531615d256b6f342f31daf35a1d4bde5a4d196
arcctgx <arcctgx@poczta.onet.pl>
2020-09-20 20:52:44 +0200

    add usage() function

--- lfmedit.sh
+++ lfmedit.sh
@@ -28,6 +28,27 @@ logError() {
     echo -e "\e[31mERR: ${FUNCNAME[1]}(): ${@}\e[0m"
 }
 
+usage() {
+    echo "usage: $(basename $0) <parameters>"
+    echo
+    echo "List of parameters:"
+    echo
+    echo "  -u <timestamp>  Unix timestamp of scrobble to edit"
+    echo "  -t <string>     new track title"
+    echo "  -a <string>     new artist name"
+    echo "  -b <string>     new album title"
+    echo "  -z <string>     new album artist name"
+    echo "  -Z <string>     original album artist name"
+    echo "  -d              increase level of debug prints"
+    echo
+    echo "Parameters -u and at least one of -t/-a/-b/-z are mandatory."
+    echo "Use -Z to override the assumption that original album artist"
+    echo "is the same as original track artist in case it's not."
+    echo
+
+    exit 1
+}
+
 parseArguments() {
     # TODO implement parsing of command-line arguments
     logDebug "args = ${@}"
