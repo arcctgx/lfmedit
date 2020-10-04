a7c1086843b3bd16d8de4b54c6f5ae0e54176ec7
arcctgx <arcctgx@poczta.onet.pl>
2020-10-05 01:50:03 +0200

    add lfmbatchedit.sh
    Introduce a script for batch processing of last.fm edits which takes
    input data from a diff file.

---
+++ lfmbatchedit.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+source "utils.sh"
+source "scrobble_edit.sh"
+
+usage() {
+    echo "usage: $(basename "$0") [-d] <file> [file2 ...]"
+    echo
+    echo "  -n  dry run: print edits, but do not apply them"
+    echo "  -Y  do not ask to confirm an edit (ignored when -n is used)"
+    echo "  -d  increase level of debug prints"
+    echo "  -h  display this help message"
+    echo
+    echo "Input files are diffs of last.fm library export files"
+    echo "compatible with the format used by lastscrape tool."
+    echo
+}
+
+parseArguments() {
+    while getopts ":nYdh" options; do
+        case "${options}" in
+            n)
+                dryRun="yes"
+                ;;
+            Y)
+                dontAsk="yes"
+                ;;
+            d)
+                ((debugLevel++))
+                ;;
+            h)
+                usage && exit 0
+                ;;
+            *)
+                # quietly ignore unsupported options
+                ;;
+        esac
+    done
+
+    if [ "${debugLevel}" -ge 2 ]; then
+        verbose="--verbose"
+        silent=""
+    else
+        verbose=""
+        silent="--silent"
+    fi
+
+    logDebug "args = ${*}"
+
+    shift $((OPTIND-1))
+    fileList="${*}"
+    logDebug "file list = ${fileList}"
+
+    if [[ ${*} == "" ]]; then
+        logError "Missing mandatory parameters!"
+        echo
+        usage
+    fi
+}
+
+applyChangesFrom() {
+    local -r file="${1}"
+    local -r nChange=$(grep -E "^\+[0-9]{10}" "${file}" | wc -l)
+    local n=0
+
+    grep -E "^\+[0-9]{10}" "${file}" | tr -d + |
+        while IFS=$'\t' read -r -a scrobble; do
+            ((n++))
+            unset -v timestamp newTitle newArtist newAlbum newAlbumArtist
+
+            logInfo "editing scrobble ${n} of ${nChange}"
+
+            timestamp="${scrobble[0]}"
+
+            requestOriginalScrobbleData || continue
+            extractOriginalScrobbleData || continue
+
+            newTitle="${scrobble[1]}"
+            newArtist="${scrobble[2]}"
+            newAlbum=${scrobble[3]}
+
+            logDebug "timestamp = $timestamp, newTitle = $newTitle, newArtist = $newArtist, newAlbum = $newAlbum"
+
+            requestScrobbleEdit || continue
+            verifyScrobbleEdit || continue
+        done
+}
+
+processFiles() {
+    local f
+
+    for f in ${fileList}; do
+        logInfo "processing \"${f}\""
+        applyChangesFrom "${f}"
+    done
+}
+
+main() {
+    parseArguments "${@}"
+    checkAuthTokens
+    processFiles
+}
+
+main "${@}"
